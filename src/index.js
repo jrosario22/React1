c2f944d186dd0737795a5184eb8ac2530d55faf4
Jeson Rosario <jesonrosario22@gmail.com>
2019-06-04 22:52:44 -0400

    first commit

--- src/index.js
+++ src/index.js
@@ -1,12 +1,40 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import "./index.css";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+class ContactCard extends React.Component{
+  render(){
+    return (
+        <div>
+        <label>Name: {this.props.name}</label>
+        <br></br>
+        <label>Mobile Phone: {this.props.mobileNum}</label>
+        <br></br>
+        <label>Work Phone: {this.props.workNum}</label>
+        <br></br>
+        <label>Email: {this.props.email}</label>
+        </div> 
+    
+    );
+  }
+}
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+const person1 = <ContactCard name="Mary"  mobileNum="212-555-5555" workNum="347-555-7777" email="mary@gmail.com"/>;
+
+const person2 = <ContactCard name="John"  mobileNum="212-555-5555" workNum="347-555-7777" email="john@gmail.com"/>;
+
+const person3 = <ContactCard name="Joseph"  mobileNum="212-555-5555" workNum="347-555-7777" email="joseph@gmail.com"/>;
+
+ReactDOM.render(
+  person1,
+  document.getElementById("person1")
+);
+ReactDOM.render(
+  person2, 
+  document.getElementById("person2")
+);
+ReactDOM.render(
+  person3,
+  document.getElementById("person3")
+);
+
