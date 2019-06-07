21c6db761fa5cb3b8ea5ef82a686c6cff9851598
Jeson Rosario <jesonrosario22@gmail.com>
2019-06-07 10:09:11 -0400

    Added logic for button increment

--- src/index.js
+++ src/index.js
@@ -19,22 +19,65 @@ class ContactCard extends React.Component{
   }
 }
 
+class Number extends React.Component{
+  constructor(props){
+      super(props)
+      this.state = {number: this.props.value};
+  }
+  increment = () => {
+    this.setState((state) => {
+      console.log(state);
+      return {number: state.number + 1};
+    });
+  }
+  decrement = () => {
+    this.setState((state) => {
+      console.log(state);
+      if(state.number <= 0){
+        return alert("Cannot be less than 0");
+      }
+      else {
+        return {number: state.number - 1};
+      }
+    });
+  }
+  render(){
+      return (
+          <div>
+              <p>{this.state.number}</p>
+              <button onClick={this.increment}>Increment</button>
+              <button onClick={this.decrement}>Decrement</button>
+          </div>
+      )
+  }
+}
+
+
 const person1 = <ContactCard name="Mary"  mobileNum="212-555-5555" workNum="347-555-7777" email="mary@gmail.com"/>;
 
 const person2 = <ContactCard name="John"  mobileNum="212-555-5555" workNum="347-555-7777" email="john@gmail.com"/>;
 
 const person3 = <ContactCard name="Joseph"  mobileNum="212-555-5555" workNum="347-555-7777" email="joseph@gmail.com"/>;
 
+let number = <Number value={10}/>;
+
 ReactDOM.render(
   person1,
   document.getElementById("person1")
 );
+
 ReactDOM.render(
   person2, 
   document.getElementById("person2")
 );
+
 ReactDOM.render(
   person3,
   document.getElementById("person3")
+);
+
+ReactDOM.render(
+  number,
+  document.getElementById("number")
 );
 
